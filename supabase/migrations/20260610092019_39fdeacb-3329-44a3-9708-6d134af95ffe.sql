d4b1330ac9aeeac5256e3dd5d0998f1673542948
solucionesnexa-dotcom <solucionesnexa@gmail.com>
2026-07-20 10:30:49 +0200

    Delete supabase/migrations/20260610092019_39fdeacb-3329-44a3-9708-6d134af95ffe.sql

--- supabase/migrations/20260610092019_39fdeacb-3329-44a3-9708-6d134af95ffe.sql
+++
@@ -1,4 +0,0 @@
-DROP POLICY IF EXISTS "Admins can add members" ON public.workspace_members;
-CREATE POLICY "Admins can add members" ON public.workspace_members
-  FOR INSERT TO authenticated
-  WITH CHECK (public.has_workspace_role(auth.uid(), workspace_id, 'admin'));
